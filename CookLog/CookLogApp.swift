7e318856b248418efad0a47dad3078115567410d
Udit Kapoor <uditkapoor060@gmail.com>
2026-03-07 00:37:34 -0800

    Initial Commit

---
+++ CookLog/CookLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookLogApp.swift
+//  CookLog
+//
+//  Created by rentamac on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CookLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
